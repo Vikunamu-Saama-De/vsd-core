5e88a5ff02360bb59d3a86a2f88c55a9eaa6aefb
pavithraRanasinghe <paviranasinghe20@gmail.com>
2025-06-22 17:22:29 +0530

    Initialize VSD

---
+++ src/main/java/com/vsd/vsdcore/VsdCoreApplication.java
@@ -0,0 +1,13 @@
+package com.vsd.vsdcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsdCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsdCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsd/vsdcore/VsdCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsd.vsdcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsdCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
